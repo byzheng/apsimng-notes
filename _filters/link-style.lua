8ea696c0aaa2d01999efb3f5526119cb9adf1ab8
Bangyou Zheng <zheng.bangyou@gmail.com>
2025-07-07 16:24:54 +1000

    Update lua script

--- _filters/link-style.lua
+++ _filters/link-style.lua
@@ -1,5 +1,9 @@
 local path = require("pandoc.path")
-local project_dir = quarto.project.directory or "."
+local function normalize_slashes(p)
+    return p:gsub("\\", "/")
+end
+
+local project_dir = normalize_slashes(quarto.project.directory or ".")
 
 
 -- Function to check if a file exists
@@ -42,10 +46,19 @@ function Link(el)
         local current_file = quarto.doc.input_file
         if current_file then
             local current_dir = path.directory(current_file)
+            -- Resolve relative path to absolute
             resolved_path = path.normalize(path.join({ current_dir, href })):gsub("\\", "/")
+            -- Then remove project_dir prefix to get relative path from project root
+            quarto.log.output("project_dir: " .. project_dir)
+            quarto.log.output("resolved_path: " .. resolved_path)
+
+            resolved_path = resolved_path:gsub("^" .. project_dir:gsub("([%.%-%+%*%?%[%]%^%$%(%)])", "%%%1") .. "/?", "")
+            -- Add leading slash to indicate project-root relative
+            resolved_path = "/" .. resolved_path
+            quarto.log.output("resolved_path_again: " .. resolved_path)
         end
     end
-    print(resolved_path)
+    -- quarto.log.output("Resolved path: " .. resolved_path)
     -- Check if the file exists
     if not file_exists(resolved_path) then
         el.attributes["class"] = "broken-link"
