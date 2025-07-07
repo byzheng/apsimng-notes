56beb668557f2e230854f5691485b9b6db2f173c
Bangyou Zheng <zheng.bangyou@gmail.com>
2025-07-07 15:08:35 +1000

    Add styles for link in croips and models

---
+++ _filters/link-style.lua
@@ -0,0 +1,29 @@
+local path = require("pandoc.path")
+
+function Link(el)
+    local href = el.target or ""
+    -- Skip external links
+    if href:match("^https?://") then
+        return el
+    end
+    local resolved_path = href
+
+    -- Resolve absolute path if href is relative
+    if href:sub(1, 1) ~= "/" and not href:match("^%a:[/\\]") then
+        local current_file = quarto.doc.input_file
+        if current_file then
+            local current_dir = path.directory(current_file)
+            resolved_path = path.normalize(path.join({ current_dir, href })):gsub("\\", "/")
+        end
+    end
+
+
+    if resolved_path:match("/docs/Models") then
+        el.attributes["class"] = "model-link"
+    elseif resolved_path:match("/docs/Crops") then
+        el.attributes["class"] = "crop-link"
+    end
+
+    return el
+end
+
