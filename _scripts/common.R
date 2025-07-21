ac3fe8dfe79ab5569569614525db09ffd4243b29
Bangyou Zheng <zheng.bangyou@gmail.com>
2025-07-21 14:54:34 +1000

    Movie example file

--- _scripts/common.R
+++ _scripts/common.R
@@ -1,4 +1,41 @@
-library(tidyverse)
-library(knitr)
+suppressPackageStartupMessages({
+    library(tidyverse)
+    library(knitr)
+    library(rapsimng)
+})
+
+read_example <- function(file, report = "DailyReport") {
+    file_path <- file.path("_examples", file) |> 
+        here::here()
+
+    if (!file.exists(file_path)) {
+        stop(paste("File not found:", file_path))
+    }
+
+    df <- rapsimng::read_report(file_path, report)
+
+    if (has_name(df, "Site")) {
+        df$Site <- gsub("\\.met$", "", df$Site)
+    }
+
+    df <- df |> 
+        dplyr::filter(Wheat.Phenology.Stage > 0)
+    return(df)
+}
 
 
+plot_output <- function(file, 
+        y, 
+        x = "Wheat.Phenology.Stage", report = "DailyReport") {
+    df <- read_example(file, report)
+
+    if (nrow(df) == 0) {
+        stop("No data found for the specified report.")
+    }
+    df |> 
+        ggplot(aes(x = .data[[x]], y = .data[[y]], color = Cultivar)) +
+        geom_line() +
+        facet_grid(Site ~ SowingDate) +
+        theme_bw() +
+        theme(legend.position = "bottom")
+}
