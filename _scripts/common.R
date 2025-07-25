b0cef028d691804dff7564f5f01a46e25f60b13a
Bangyou Zheng <zheng.bangyou@gmail.com>
2025-07-25 11:55:01 +1000

    Plot the vrn1

--- _scripts/common.R
+++ _scripts/common.R
@@ -54,17 +54,47 @@ read_example <- function(file, report = "DailyReport") {
 
 plot_output <- function(file, 
         y, 
-        x = "Wheat.Phenology.Stage", report = "DailyReport") {
+        x = "Wheat.DaysAfterSowing", report = "DailyReport",
+        max_stage = NULL,
+        stage_name = TRUE) {
+    stopifnot(is.character(file) && length(file) == 1)
+    stopifnot(is.character(y) && length(y) == 1)
+    stopifnot(is.character(x) && length(x) == 1)
+    stopifnot(is.character(report) && length(report) == 1)
+    stopifnot(is.logical(stage_name) && length(stage_name) == 1)
+
     df <- read_example(file, report)
 
     if (nrow(df) == 0) {
         stop("No data found for the specified report.")
     }
-    df |> 
+    if (!is.null(max_stage)) {
+        stopifnot(length(max_stage) == 1)
+        stopifnot(is.numeric(max_stage))
+        df <- df |> 
+            dplyr::filter(Wheat.Phenology.Stage <= max_stage)
+    }
+    p <- df |> 
         ggplot2::ggplot(ggplot2::aes(x = .data[[x]], y = .data[[y]], color = Cultivar)) +
         ggplot2::geom_line() +
         ggplot2::geom_point(ggplot2::aes(shape = Cultivar)) +
         ggplot2::facet_grid(Site ~ SowingDate) +
         ggplot2::theme_bw() +
         ggplot2::theme(legend.position = "bottom")
+    if (stage_name) {
+        
+        pd_label <- df |> 
+            dplyr::mutate(y = min(.data[[y]], na.rm = TRUE)) |>
+            dplyr::select(all_of(c("Site", "SowingDate", "Cultivar", x, 'Wheat.Phenology.CurrentStageName', "y"))) |>     
+            dplyr::filter(nchar(Wheat.Phenology.CurrentStageName) > 0)
+        p <- p + 
+            ggplot2::geom_text(data = pd_label, 
+                            ggplot2::aes(y = y, label = Wheat.Phenology.CurrentStageName), 
+                            vjust = 0.5, 
+                            angle = 90,
+                            hjust = 0, 
+                            size = 3.5) +
+            ggplot2::labs(x = x, y = y)
+    }
+    return (p)
 }
