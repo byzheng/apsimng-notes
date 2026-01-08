1ed3f0dfe8912ac79a4d244ee7a49ed602be8961
Bangyou Zheng <zheng.bangyou@gmail.com>
2026-01-08 14:20:18 +1000

    Update batch files

---
+++ preview.bat
@@ -0,0 +1,8 @@
+@echo off
+setlocal
+
+REM Preview Quarto Project
+
+quarto preview --port 7859
+
+endlocal
---
+++ render.bat
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+
+REM Render Quarto Project
+
+quarto render
+
+echo Render complete.
+endlocal
